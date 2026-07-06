086bc32cd86ce4f3a27cdc7cd5002556e5620d7f
mp35156-creator <mp35156@yahoo.com>
2026-07-05 22:52:14 -0400

    Update Site-data.js

--- Site-data.js
+++ Site-data.js
@@ -1,1 +1,10 @@
+/*
+    Longview Parkinson's Support Group
+    Site Data
 
+    This file is reserved for future updates.
+
+    In a future version, meeting information, contact information,
+    announcements, and guest speakers will be stored here so the
+    website can be updated without editing index.html.
+*/
